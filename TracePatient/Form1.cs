1d629c186e085b43ef86b263655976fdd513a14a
Wang Jeremy <wj982397072@163.com>
2015-11-27 01:14:20 +0800

    Add new project patient and desgin the forms

---
+++ TracePatient/Form1.cs
@@ -0,0 +1,41 @@
+﻿using ComponentFactory.Krypton.Toolkit;
+
+namespace TracePatient
+{
+    public partial class TracePatientForm : KryptonForm
+    {
+        public TracePatientForm()
+        {
+            InitializeComponent();
+        }
+
+        private void kryptonGroupBox1_Panel_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+
+        }
+
+        private void txtNurse_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void detailsPanel_Resize(object sender, System.EventArgs e)
+        {
+            this.detailsPanel.Width = 300;
+        }
+
+        private void splitContainer_Panel1_Resize(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void splitContainer_Panel1_SizeChanged(object sender, System.EventArgs e)
+        {
+        }
+    }
+}
