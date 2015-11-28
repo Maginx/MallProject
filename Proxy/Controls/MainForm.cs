85599e4a5f514ddce6425f54a48dbb74a8143ce8
Wang Jeremy <wj982397072@163.com>
2015-11-29 01:17:13 +0800

    Tag 1.0 version

--- Proxy/Controls/MainForm.cs
+++ Proxy/Controls/MainForm.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using ComponentFactory.Krypton.Toolkit;
 using System.Runtime.InteropServices;
-using System.IO;
-using System.Xml.Linq;
-using System.Net;
 using ClassLibrary;
-using System.Threading;
 
 namespace ProxyClient.Controls
 {
